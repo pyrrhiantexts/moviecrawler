c1289b7b5f0300e3af86cf986dbf9f67342507a9
Elysia <ElysiaRamirezHasAnEmail@gmail.com>
2025-03-14 14:30:57 -0700

    Learning

--- moviecrawler/spiders/cinema21.py
+++ moviecrawler/spiders/cinema21.py
@@ -9,5 +9,7 @@ class Cinema21Spider(scrapy.Spider):
         yield scrapy.Request(url = url, callback = self.parse) 
 
     def parse(self, response):
-        page=response.url.split('/')[-3]
-        title = response.selector.xpath('//div[contains(@class, "movie-info")//h2]')
+        #Ok, so this is wrong, although my selector was mostly written correctly!
+        #I'm actually pulling from a Javascript thingy. SO I would need to parse it out of javascript first.
+        #title = response.css(div.movieData/title::text).getall(Default='Nothing Found')
+        
