faf2ea0dcd7cb7ce78340b55b0844c7c1e4e9501
Elysia <ElysiaRamirezHasAnEmail@gmail.com>
2025-03-14 15:58:43 -0700

    First edition of regex JS parsing

--- moviecrawler/spiders/cinema21.py
+++ moviecrawler/spiders/cinema21.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 
 class Cinema21Spider(scrapy.Spider):
@@ -9,7 +10,9 @@ class Cinema21Spider(scrapy.Spider):
         yield scrapy.Request(url = url, callback = self.parse) 
 
     def parse(self, response):
+        titles = response.css('//script/text()').re(".*'title': '(\w*)'")
         #Ok, so this is wrong, although my selector was mostly written correctly!
         #I'm actually pulling from a Javascript thingy. SO I would need to parse it out of javascript first.
         #title = response.css(div.movieData/title::text).getall(Default='Nothing Found')
+        return titles
         
