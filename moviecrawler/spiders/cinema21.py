410e2ac28135aea6d2e5babd59aee11f496f0a17
Elysia <ElysiaRamirezHasAnEmail@gmail.com>
2025-03-16 14:40:58 -0700

    Fix selector and regex

--- moviecrawler/spiders/cinema21.py
+++ moviecrawler/spiders/cinema21.py
@@ -7,10 +7,10 @@ class Cinema21Spider(scrapy.Spider):
     allowed_domains = ["www.cinema21.com"]
     start_urls = ["https://www.cinema21.com"]
     for url in start_urls:
-        yield scrapy.Request(url = url, callback = self.parse) 
+        response = scrapy.Request(url = url)
 
     def parse(self, response):
-        titles = response.css('//script/text()').re(".*'title': '(\w*)'")
+        titles = response.css('script::text').re(r".*'title': '(.*)'")
         #Ok, so this is wrong, although my selector was mostly written correctly!
         #I'm actually pulling from a Javascript thingy. SO I would need to parse it out of javascript first.
         #title = response.css(div.movieData/title::text).getall(Default='Nothing Found')
