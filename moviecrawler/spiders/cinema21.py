8e86612ca3de5700377eb6cff05c469b6e1c7645
Elysia <ElysiaRamirezHasAnEmail@gmail.com>
2025-03-16 15:19:19 -0700

    Revise selector

--- moviecrawler/spiders/cinema21.py
+++ moviecrawler/spiders/cinema21.py
@@ -10,7 +10,8 @@ class Cinema21Spider(scrapy.Spider):
         response = scrapy.Request(url = url)
 
     def parse(self, response):
-        titles = response.css('script::text').re(r".*'title': '(.*)'")
+
+        titles = response.css('script:nth-of-type(6)::text').re(r".*'title': '(.*)'")
         #Ok, so this is wrong, although my selector was mostly written correctly!
         #I'm actually pulling from a Javascript thingy. SO I would need to parse it out of javascript first.
         #title = response.css(div.movieData/title::text).getall(Default='Nothing Found')
