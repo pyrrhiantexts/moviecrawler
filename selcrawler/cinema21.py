091c5417a7004ce50e6bbb34c5c288426e29b282
Elysia <ElysiaRamirezHasAnEmail@gmail.com>
2025-03-22 13:49:46 -0700

    Set headless browser

--- selcrawler/cinema21.py
+++ selcrawler/cinema21.py
@@ -1,9 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
-driver.get("https://www.cinema21.com")
+#this option makes the browser 'headless' which means Chrome doesn't
+#briefly open to pull the HTML
+options = Options()
+options.add_argument("--headless")
 
-movie_titles = driver.find_elements(By.CSS_SELECTOR, ".movie-info h2")
-for title in movie_titles:
-    print(title.text)
+#this defines which browser to use and runs the request to pull
+driver = webdriver.Chrome(chrome_options=options)
+
+def cinema21():
+
+    driver.get("https://www.cinema21.com")
+
+    movie_titles = driver.find_elements(By.CSS_SELECTOR, ".movie-info h2")
+    for title in movie_titles:
+        print(title.text)
+
+def hollywood():
+    driver.get("https://www.hollywoodtheatre.com")
+    hollywood_titles = driver.find_elements(By.CSS_SELECTOR, #TODO)
+                                            
