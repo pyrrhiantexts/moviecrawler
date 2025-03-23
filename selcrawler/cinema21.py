44652336b3750d8355b3bef5f101e2dcb9242583
Elysia <ElysiaRamirezHasAnEmail@gmail.com>
2025-03-22 17:51:23 -0700

    Introduce SQLite

--- selcrawler/cinema21.py
+++ selcrawler/cinema21.py
@@ -1,24 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import sqlite3
 
 #this option makes the browser 'headless' which means Chrome doesn't
 #briefly open to pull the HTML
-options = Options()
-options.add_argument("--headless")
+chrome_options = Options()
+chrome_options.add_argument("--headless=new")
 
 #this defines which browser to use and runs the request to pull
-driver = webdriver.Chrome(chrome_options=options)
+driver = webdriver.Chrome(options=chrome_options)
+c21_titles = []
 
 def cinema21():
-
     driver.get("https://www.cinema21.com")
+    c21_titles = driver.find_elements(By.CSS_SELECTOR, ".movie-info h2")
+    print(c21_titles)
 
-    movie_titles = driver.find_elements(By.CSS_SELECTOR, ".movie-info h2")
-    for title in movie_titles:
-        print(title.text)
 
-def hollywood():
-    driver.get("https://www.hollywoodtheatre.com")
-    hollywood_titles = driver.find_elements(By.CSS_SELECTOR, #TODO)
-                                            
+#def hollywood():
+    #driver.get("https://www.hollywoodtheatre.org")
+    #hollywood_titles = driver.find_elements(By.CSS_SELECTOR, "#TODO")
+
+def tables():
+    conn = sqlite3.connect('Database/Films.db')
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS films(title)")
+    for item in c21_titles:
+        command = ("INSERT INTO films VALUES(" + item.text + ")")
+        cur.execute(command)
+
+if __name__ == '__main__':
+    cinema21()
+    tables()
